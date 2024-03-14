c08f1130aa9ad20022bd5c4cb5415911259205fb
Miguel Panuto <miguel.panuto@live.com>
2024-03-13 22:16:04 -0300

    fix: create gcp keys at runtime

--- Dockerfile
+++ Dockerfile
@@ -6,6 +6,9 @@ COPY requirements.txt ./
 
 RUN pip install -r requirements.txt
 
+COPY create_gcp_keys.py ./
+RUN python create_gcp_keys.py
+
 COPY . .
 
 ENV GOOGLE_APPLICATION_CREDENTIALS=/usr/src/app/gcp-keys.json
--- __main__.py
+++ __main__.py
@@ -8,23 +8,13 @@ from src.infra.clients import instantiate_gcp_storage_client, instantiate_ai_cli
 
 import asyncio
 import sys
-from os import environ
 from paths import PROJECT_ROOT 
 
 
-def create_gcp_keys():
-    gcp_json_plain = environ.get('GCP_KEYS_JSON')
-    print(gcp_json_plain)
-    with open('gcp-keys.json', 'w', encoding='utf-8') as f:
-        if gcp_json_plain is None:
-            print('GCP_KEYS_JSON is not set')
-            return
-        f.write(gcp_json_plain)
 
 def main():
     sys.path.insert(1, PROJECT_ROOT)
     load_dotenv()
-    create_gcp_keys()
 
     instantiate_database_engine()
     instantiate_gcp_storage_client()
